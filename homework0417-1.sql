9f112f0f0b51c117e08e5436d7ce46da623c3b36
Eunhee-nim <126763448+Eunhee-nim@users.noreply.github.com>
2023-04-18 18:15:15 +0900

    그룹함수 예제

---
+++ homework0417-1.sql
@@ -0,0 +1,41 @@
+--1) 각 학과별 학년별 학생 수를 ROLLUP함수로 검색하세요
+SELECT MAJOR
+    , SYEAR
+    , COUNT(*)
+    FROM STUDENT 
+    GROUP BY ROLLUP (MAJOR, SYEAR);
+
+--2) 화학과와 생물학과 학생 4.5 환산 평점의 평균을 각각 검색하는 데 
+--화학과 생물이 열로 만들어지도록 하세요.(PIVOT 사용)
+SELECT *
+    FROM (SELECT MAJOR
+            , AVR 
+            FROM STUDENT
+            ) PIVOT (AVG(AVR*(4.5/4))
+                FOR MAJOR IN (
+                    '화학' 화학
+                    , '생물' 생물
+                        )
+                    );
+    
+
+--3) 학과별 학생이름을 ,로 구분하여 성적순으로(내림차순) 조회하세요.(LISTAGG 사용)
+SELECT AVR 
+    , MAJOR
+    , LISTAGG (SNAME, ', ')
+    FROM STUDENT
+    GROUP BY AVR, MAJOR
+    ORDER BY AVR DESC;
+
+
+--4) 부서별 업무별 연봉의 평균을 검색하세요
+--(부서와 업무 컬럼의 그룹화 여부도 같이 검색, GROUPING 사용)
+SELECT GROUPING(D.DNAME) 부서그룹여부
+        , D.DNAME
+        , GROUPING(E.JOB)업무그룹여부
+        , E.JOB
+        , ROUND(AVG(E.SAL),2)
+        FROM EMP E
+        JOIN DEPT D
+        ON E.DNO = D.DNO
+        GROUP BY D.DNAME, E.JOB;
